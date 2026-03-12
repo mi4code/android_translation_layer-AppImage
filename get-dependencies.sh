713aa93012e340ffaf233330a1f4a448838ef130
mi4code <114027049+mi4code@users.noreply.github.com>
2026-03-12 13:30:29 +0100

    use yay

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,5 +21,6 @@ echo "---------------------------------------------------------------"
 #sudo pacman -Syy --noconfirm
 #sudo pacman -S --noconfirm chaotic-keyring chaotic-mirrorlist
 sudo pacman -Syy --noconfirm
-
-make-aur-package --chaotic-aur android_translation_layer-git
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S android_translation_layer-git
+#make-aur-package --chaotic-aur android_translation_layer-git
