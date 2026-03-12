c1e8808c86879ceb783ba36bf3e2df3396f48e62
mi4code <114027049+mi4code@users.noreply.github.com>
2026-03-12 12:21:53 +0100

    attempt to fix 404 error

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,4 +13,25 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-mesa libxml2-mini opus-mini gdk-pixbuf2-mini librsvg-mini
 
 # Comment this out if you need an AUR package
+echo "Making AUR package..."
+echo "---------------------------------------------------------------"
+
+sudo pacman -Rdd --noconfirm chaotic-keyring chaotic-mirrorlist 2>/dev/null || true
+# Install latest keyring
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
+# Install latest mirrorlist
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+# Refresh pacman databases
+sudo pacman -Rdd --noconfirm chaotic-keyring chaotic-mirrorlist 2>/dev/null || true
+# Install latest keyring
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
+# Install latest mirrorlist
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+# Refresh pacman databases
+sudo pacman -Syy
+
 make-aur-package --chaotic-aur android_translation_layer-git
