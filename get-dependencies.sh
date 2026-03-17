88962c65b0b03a0cb8af0dc04ed6df0e2eb342c7
mi4code <114027049+mi4code@users.noreply.github.com>
2026-03-17 15:39:43 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,42 +17,33 @@ echo "Making AUR package..."
 echo "---------------------------------------------------------------"
 
 # ---- Initialize and refresh pacman keys ----
-
-#sudo pacman -Syy --noconfirm
-#sudo pacman -S --noconfirm chaotic-keyring chaotic-mirrorlist
 sudo pacman -Syy --noconfirm
+#sudo pacman -S --noconfirm chaotic-keyring chaotic-mirrorlist
 
-# create a non-root user for building AUR packages
+# create a non-root user for building AUR packages + allow sudo without password
 sudo useradd -m builder
 sudo passwd -d builder
-
-# allow sudo without password
 echo "builder ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/builder
-sudo pacman -S --noconfirm go
-# build yay as the builder user
+
+# run as the builder user
 sudo -u builder bash <<'EOF'
 cd /home/builder
-sudo pacman -S --noconfirm --needed wget git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 
-# install libvixl (use debian as source since not availible on arch; needed on arm)
+# build yay
+sudo pacman -S --noconfirm --needed go wget git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
+
+# install libvixl (use debian as source cos not availible on arch; needed on arm)
 yay -S --noconfirm dpkg
 wget http://ftp.de.debian.org/debian/pool/main/v/vixl/libvixl5_5.1.0-6+b1_arm64.deb
 wget http://ftp.de.debian.org/debian/pool/main/v/vixl/libvixl-dev_5.1.0-6+b1_arm64.deb
 sudo dpkg --force-depends --install libvixl5_*.deb
 sudo dpkg --force-depends --install libvixl-dev_*.deb
 sudo dpkg --configure -a
-echo "-----lib:"
-ls /usr/lib/*vixl*
-echo "-----lib-aarch:"
-ls /usr/lib/aarch64-linux-gnu/*vixl*
-#sudo cp /usr/lib/aarch64-linux-gnu/*vixl* /usr/lib/
-#echo "/usr/lib/aarch64-linux-gnu" | sudo tee /etc/ld.so.conf.d/vixl.conf
-#sudo ldconfig
 sudo ln -s /usr/lib/aarch64-linux-gnu/libvixl.so /usr/lib/libvixl.so
 sudo ln -s /usr/lib/aarch64-linux-gnu/libvixl.so.5 /usr/lib/libvixl.so.5
 sudo ln -s /usr/lib/aarch64-linux-gnu/libvixl.so.5.1.0 /usr/lib/libvixl.so.5.1.0
 
-# build without skia (no longer dependency, but still required by the aur package)
+# build without skia (no longer dependency, but still required by the aur package and wont build)
 #yay -S --noconfirm android_translation_layer-git
 yay -S --noconfirm \
   art_standalone \
@@ -61,9 +52,8 @@ yay -S --noconfirm \
 yay -G android_translation_layer-git
 cd android_translation_layer-git
 sed -i '/skia-sharp-atl/d' PKGBUILD
-JAVA_TOOL_OPTIONS="-Xmx1536m" _JAVA_OPTIONS="-Xmx1536m" MAKEFLAGS="-j1" NINJAFLAGS="-j1" makepkg -si --noconfirm
+makepkg -si --noconfirm
+
 EOF
-
-
 
 #make-aur-package --chaotic-aur android_translation_layer-git
